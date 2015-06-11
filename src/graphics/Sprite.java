ba63b1f1e8dab40fb370556adcda9a6ac7a4c277
JonathanZwiebel <jonathanzwiebel@gmail.com>
2015-06-11 16:06:01 -0700

    Filled in Sprite class

--- src/graphics/Sprite.java
+++ src/graphics/Sprite.java
@@ -1,5 +1,33 @@
 package graphics;
 
-public class Sprite {
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
+
+public class Sprite {
+	private String imageLocation, name;
+	private File imageFile;
+	private BufferedImage image;
+	
+	public Sprite(String name) {
+		this.name = name;
+	}
+	
+	public Sprite(String name, String imageLocation) {
+		this(name);
+		loadSprite(imageLocation);
+	}
+	
+	private void loadSprite(String imageLocation) {
+		this.imageLocation = imageLocation;
+		imageFile = new File(imageLocation);
+		try {
+			image = ImageIO.read(imageFile);
+		} catch (IOException e) {
+			System.out.println("Illegal path for " + name + " : " + imageLocation);
+			e.printStackTrace();
+		}
+	}
 }
