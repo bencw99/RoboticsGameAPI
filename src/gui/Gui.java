498f46a6d72842009b862bd118912277a7c1d3bb
Nihar Mitra <nihar.roy.mitra@gmail.com>
2015-06-12 16:12:24 -0700

    Adding in general gui class

---
+++ src/gui/Gui.java
@@ -0,0 +1,21 @@
+package gui;
+
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+public class Gui extends JFrame {
+	private static ArrayList<Button> buttons = new ArrayList<Button>();
+	/**
+	 * Default serial ID
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public Gui() {
+		
+	}
+	
+	public void buttonClicked() {
+		
+	}
+}
