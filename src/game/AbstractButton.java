fe2139aaa53fd03600d67088dce3b5a00709a64d
orianL <2021oleitersdorf@jerusalemais.org>
2015-06-15 17:15:49 -0700

    Finished example

--- src/game/AbstractButton.java
+++ src/game/AbstractButton.java
@@ -3,10 +3,9 @@ package game;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import gui.*;
-public abstract class AbstractButton implements ActionListener {
+public abstract class AbstractButton {
 	private Button jbutton;
 	private String name;
-	public abstract void actionPerformed(ActionEvent e);
 	public abstract void init();
 	public Button getButton(){
 		return jbutton;
