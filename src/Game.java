b75e1e94d8eb54bf14db78e66431349b9959c262
orianL <2021oleitersdorf@jerusalemais.org>
2015-07-22 18:36:55 -0700

    worked on new game file

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,10 @@
 //Paly Robotics - Team 8
 //Game API
 
-package game;
+import input.InputListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Game extends JPanel{
 	/**
@@ -20,4 +23,32 @@ public class Game extends JPanel{
 	 *	A frame that will contain the game
 	**/
 	private JFrame frame;
+	/**
+	 * Input Listener
+	 */
+	private InputListener inputListener;
+	/**
+	 * Default constructor, creates an empty world
+	 */
+	public Game(){
+		initFrame();
+	}
+	/**
+	 * Initializes frame
+	 */
+	public void initFrame(){
+		inputListener = new InputListener();
+		frame = new JFrame();
+		frame.setTitle("Paly Robotics Team 8 - Programming - Game API");
+		frame.setSize(1000, 1000);
+		frame.add(this);
+		frame.setVisible(true);
+		frame.setLayout(null);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(inputListener);
+		frame.addMouseListener(inputListener);
+		
+	}
 }
