96326877403906496b58f386f7546600c0b62fd1
Driver Station <jngo@palyrobotics.com>
2015-07-23 16:55:45 -0700

    started abstract text box

---
+++ src/addons/AbstractTextBox.java
@@ -0,0 +1,42 @@
+package addons;
+
+import physics.Position;
+
+public class AbstractTextBox implements NonEntityElement{
+
+	public void init() {
+	}
+
+	public void update() {	
+	}
+
+	public void disable() {	
+	}
+
+	public void setPosition(Position p) {	
+	}
+
+	public void setPosition(double xPos, double yPos) {	
+	}
+	
+	public Position getPosition() {
+		return null;
+	}
+
+	public double getXPosition() {
+
+		return 0;
+	}
+
+	public double getYPosition() {
+		return 0;
+	}
+
+	public void show() {
+	}
+
+	public void hide() {
+		
+	}
+
+}
