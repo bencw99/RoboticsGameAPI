eeb7a27c73374258fb1bbf8d100e91d36cfad701
orianL <2021oleitersdorf@jerusalemais.org>
2015-07-22 19:38:24 -0700

    Revert "update"
    This reverts commit 81bb16624a3653d17acccfd697a3e98ca9e19924.

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,3 +1,112 @@
+package game;
+import javax.swing.JFrame;
+import constants.Constants;
+import implementaion.Implementor;
+import input.*;
+import world.*;
+import gui.*;
+
+/**
+ * The class that starts up the game
+ */
+public class Game{
+	//A Listener for this.buttonClicked(Button caller)
+	private Listener<gui.Button> buttonClickedListenerInstance = new ButtonClickedListener<gui.Button>(this);
+	
+	/** The state enum of the game class **/
+	public static enum State {
+		LOADING,
+		RUNNING,
+		PAUSED,
+		FINISHED
+	}
+	
+	/** The world associated with this game **/
+	private World world;
+	
+	/** The state of this game **/
+	private State state;
+	
+	/** A JPanel used to hold the entities **/
+	private Gui GUI;
+	
+	/** A JFrame that will contain GUI **/
+	private JFrame frame;
+
+	/** 
+	 * Default constructor, creates an empty world
+	 */
+	public Game(Implementor imp) {	
+		this.world = new World();
+		this.state = State.LOADING;
+		GUI = new Gui(this);
+		frame = new JFrame();
+		frame.setTitle("Game");
+		frame.setSize(1000, 1000);
+		frame.add(GUI);
+		frame.setVisible(true);
+		frame.setLayout(null);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(GUI.getListener());
+
+		this.state = State.RUNNING;
+	}
+	
+	
+	public void main() {
+		while(state != State.FINISHED) {
+			update();	
+			try {
+				Thread.sleep(1000 / (int) Constants.UPDATES_PER_SEC);
+			} catch (InterruptedException e) { 
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/** 
+	 * Updates this game
+	 */
+	public void update() {
+		//Different keys pressed to start stuff
+				
+		switch(state) {
+		case LOADING:
+			if(InputListener.isKeyPressed(' ')) {
+				state = State.RUNNING;
+			}
+			break;
+		case RUNNING:
+			if(InputListener.isKeyNewPressed('p')) {
+				state = State.PAUSED;
+			}
+			world.update();
+			// GUI.update will draw all the entities
+			GUI.update(world.getButtons());
+			break;
+		case PAUSED:
+			if(InputListener.isKeyPressed(' ')) {
+				state = State.RUNNING;
+			}
+			break;
+		case FINISHED: 
+			world.disable();
+			break;
+			//TODO Graphically close the game
+		}
+	}
+	
+	public void buttonClicked(AbstractButton button){
+		
+	}
+	/**
+	 * @return the world object
+	 */
+	public World getWorld() {
+		return world;
+	}
 
 	/**
 	 * @return the state of the game
