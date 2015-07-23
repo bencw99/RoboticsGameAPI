84796d71f6410206897763186df82345e2f4a3b7
orianL <2021oleitersdorf@jerusalemais.org>
2015-07-22 18:23:31 -0700

    Working on new Game file

---
+++ src/Game.java
@@ -0,0 +1,23 @@
+//Paly Robotics - Team 8
+//Game API
+
+package game;
+
+public class Game extends JPanel{
+	/**
+		The possible states the game can be in
+	**/
+	public static enum State{
+		RUNNING,
+		PAUSED,
+		FINISHED
+	}
+	/**
+		The current state of the game
+	**/
+	private State state;
+	/**
+		
+	**/
+	
+}
