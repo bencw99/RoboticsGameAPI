069389e14ec3299ff34444abb82592112039d7f9
Eric Liu <ericxliu@gmail.com>
2015-07-24 14:01:29 -0700

    finished abstract text box

--- src/addons/AbstractTextBox.java
+++ src/addons/AbstractTextBox.java
@@ -2,41 +2,76 @@ package addons;
 
 import physics.Position;
 
-public class AbstractTextBox implements NonEntityElement{
+import java.awt.*;
+
+public abstract class AbstractTextBox extends TextArea implements NonEntityElement{
+
+    private TextArea text;
+
+    private Position p;
+
+    private double xPos;
+    private double yPos;
+
+    private int numberOfRows;
+    private int numberOfColumns;
+
+    private String hiddenText;
+
+    public abstract void update();
+
+    public AbstractTextBox(String s) {
+        text = new TextArea(s);
+    }
+
+    public AbstractTextBox() {
+        text = new TextArea("");
+    }
+
+    public AbstractTextBox(int numberOfRows, int numberOfColumns) {
+        text = new TextArea(numberOfRows, numberOfColumns);
+    }
+
+    public AbstractTextBox(String text, int numberOfRows, int numberOfColumns) {
+        this.text = new TextArea(text, numberOfRows, numberOfColumns);
+    }
 
 	public void init() {
+        text.setEnabled(true);
 	}
 
-	public void update() {	
+	public void disable() {
+        text.setEnabled(false);
 	}
 
-	public void disable() {	
+	public void setPosition(Position p) {
+        this.p = p;
 	}
 
-	public void setPosition(Position p) {	
-	}
-
-	public void setPosition(double xPos, double yPos) {	
+	public void setPosition(double xPos, double yPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
 	}
 	
 	public Position getPosition() {
-		return null;
+		return this.p;
 	}
 
 	public double getXPosition() {
-
-		return 0;
+        return this.xPos;
 	}
 
 	public double getYPosition() {
-		return 0;
+		return this.yPos;
 	}
 
 	public void show() {
+        text.setText(hiddenText);
 	}
 
 	public void hide() {
-		
+        hiddenText = text.getText();
+		text.setText("");
 	}
 
 }
